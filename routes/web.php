331f0e5110b33e83fb4f74e27274a9f6fba1e911
Afiq Firdaus <fafiq8445@gmail.com>
2025-02-24 20:18:00 +0700

    jobsheet02

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,80 @@
 <?php
 
 use Illuminate\Support\Facades\Route; // Import Route
-use App\Http\Controllers\ItemController; // Import ItemController
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () { // Route / untuk mengembalikan view welcome
-    return view('welcome');
+//JOBSHEET02 Route
+Route::get('/hello', function () {
+    return 'Hello World';
 });
 
-Route::resource('items', ItemController::class); // Route /items untuk mengelola item yang berisikan create, store, show, edit, update, dan destroy
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM  : 2341760189, Nama : Muhammad Afiq Firdaus';
+});
+
+Route::get('/user/{Afiq}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($articlesid) {
+    return 'Halaman Artikel dengan ID: ' . $articlesid;
+});
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya : ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+Route::get(
+    '/user/profile',
+    [UserProfileController::class, 'show']
+)->name('profile');
+
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::middleware('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+Route::redirect('/here', '/there');
