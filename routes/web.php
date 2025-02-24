b10b0dbd312ab23e1089804c5edd610cafabc7e0
Afiq Firdaus <fafiq8445@gmail.com>
2025-02-24 23:50:52 +0700

    Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route; // Import Route
 use App\Http\Controllers\UserProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
 
 //JOBSHEET02 Route
 Route::get('/hello', function () {
@@ -15,13 +21,13 @@ Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () {
-    return 'NIM  : 2341760189, Nama : Muhammad Afiq Firdaus';
-});
+// Route::get('/about', function () {
+//     return 'NIM  : 2341760189, Nama : Muhammad Afiq Firdaus';
+// });
 
 Route::get('/user/{Afiq}', function ($name) {
     return 'Nama saya ' . $name;
@@ -31,9 +37,9 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 });
 
-Route::get('/articles/{id}', function ($articlesid) {
-    return 'Halaman Artikel dengan ID: ' . $articlesid;
-});
+// Route::get('/articles/{id}', function ($articlesid) {
+//     return 'Halaman Artikel dengan ID: ' . $articlesid;
+// });
 
 Route::get('/user/{name?}', function ($name = null) {
     return 'Nama saya : ' . $name;
@@ -77,7 +83,20 @@ Route::middleware('admin')->group(function () {
     Route::get('/post', [PostController::class, 'index']);
     Route::get('/event', [EventController::class, 'index']);
 });
+
 Route::redirect('/here', '/there');
 
 Route::view('/welcome', 'welcome');
 Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+Route::get('/hello', [WelcomeController::class,'hello']);
+
+// Route::get('/', [PageController::class, 'index']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
+
+Route::resource('photos', PhotoController::class);
